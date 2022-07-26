44841a7d803604340aa6b768ee6c3e61b92cf5f6
Jason Doughty <jdoughty@hollertaxi.com>
2022-07-26 08:38:55 +0100

    Testing

--- ios/MapboxNavigationFreeDriveView.swift
+++ ios/MapboxNavigationFreeDriveView.swift
@@ -3,38 +3,13 @@ import MapboxNavigation
 import MapboxDirections
 import MapboxMaps
 
-// // adapted from https://pspdfkit.com/blog/2017/native-view-controllers-and-react-native/ and https://github.com/mslabenyak/react-native-mapbox-navigation/blob/master/ios/Mapbox/MapboxNavigationView.swift
-extension UIView {
-  var parentViewController: UIViewController? {
-    var parentResponder: UIResponder? = self
-    while parentResponder != nil {
-      parentResponder = parentResponder!.next
-      if let viewController = parentResponder as? UIViewController {
-        return viewController
-      }
-    }
-    return nil
-  }
-}
-
 class MapboxNavigationFreeDriveView: UIView, NavigationMapViewDelegate, NavigationViewControllerDelegate {
   var navigationMapView: NavigationMapView!
   var navigationRouteOptions: NavigationRouteOptions!
   var embedded: Bool
   var embedding: Bool
   
-  @objc var origin: NSArray = []
-  @objc var destination: NSArray = []
-  @objc var shouldSimulateRoute: Bool = false
-  @objc var showsEndOfRouteFeedback: Bool = false
-  @objc var hideStatusView: Bool = false
-  @objc var mute: Bool = false
-  
   @objc var onLocationChange: RCTDirectEventBlock?
-  @objc var onRouteProgressChange: RCTDirectEventBlock?
-  @objc var onError: RCTDirectEventBlock?
-  @objc var onCancelNavigation: RCTDirectEventBlock?
-  @objc var onArrive: RCTDirectEventBlock?
   
   override init(frame: CGRect) {
     self.embedded = false
@@ -136,21 +111,5 @@ class MapboxNavigationFreeDriveView: UIView, NavigationMapViewDelegate, Navigati
   
   func navigationViewController(_ navigationViewController: NavigationViewController, didUpdate progress: RouteProgress, with location: CLLocation, rawLocation: CLLocation) {
     onLocationChange?(["longitude": location.coordinate.longitude, "latitude": location.coordinate.latitude])
-    onRouteProgressChange?(["distanceTraveled": progress.distanceTraveled,
-                            "durationRemaining": progress.durationRemaining,
-                            "fractionTraveled": progress.fractionTraveled,
-                            "distanceRemaining": progress.distanceRemaining])
-  }
-  
-  func navigationViewControllerDidDismiss(_ navigationViewController: NavigationViewController, byCanceling canceled: Bool) {
-    if (!canceled) {
-      return;
-    }
-    onCancelNavigation?(["message": ""]);
-  }
-  
-  func navigationViewController(_ navigationViewController: NavigationViewController, didArriveAt waypoint: Waypoint) -> Bool {
-    onArrive?(["message": ""]);
-    return true;
   }
 }
