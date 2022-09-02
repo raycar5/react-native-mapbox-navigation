fda2ddc7a4cb0699390a130cdfcf9a7c75259d75
Jason Doughty <jdoughty@hollertaxi.com>
2022-09-02 13:06:02 +0100

    Latest

--- ios/MapboxNavigationFreeDriveView.swift
+++ ios/MapboxNavigationFreeDriveView.swift
@@ -140,7 +140,7 @@ class MapboxNavigationFreeDriveView: UIView, NavigationMapViewDelegate, Navigati
     }
   }
 
-  @objc func showRoute(origin: [NSNumber], destination: [NSNumber], waypoints: [[NSNumber]], styles: [NSDictionary], legIndex: NSNumber, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+  @objc func showRoute(origin: [NSNumber], destination: [NSNumber], waypoints: [[NSNumber]], styles: [NSDictionary], legIndex: NSNumber, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
     currentOrigin = origin
     currentDestination = destination
     currentWaypoints = waypoints
@@ -172,7 +172,7 @@ class MapboxNavigationFreeDriveView: UIView, NavigationMapViewDelegate, Navigati
       Directions.shared.calculate(options) { [weak self] (_, result) in
         switch result {
           case .failure(let error):
-            reject(["error": true])
+            reject(@"error", @"failed", ["message": error.localizedDescription])
             //print(error.localizedDescription)
           case .success(let response):
             guard let self = self else { return }
